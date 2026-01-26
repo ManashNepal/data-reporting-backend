fe2e661a1c41358a2db277b052cdeb3929657f9f
ManashNepal <manash.nepal111@gmail.com>
2026-01-26 12:45:10 +0545

    User schemas and models

---
+++ app/schemas/user.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class UserBase(BaseModel):
+    email : EmailStr
+    password : str
+
+# Signup
+class UserCreate(UserBase):
+    pass
+
+# Login
+class UserLogin(UserBase):
+    pass 
+
+class UserResponse(BaseModel):
+    id : int 
+    email : EmailStr
+    role : str 
+    isactive: bool
+
+    model_config = ConfigDict(from_attributes = True)
+
+
