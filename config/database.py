7d16055a032d418d28c01558ccdb30c5b8cd9591
ManashNepal <manash.nepal111@gmail.com>
2026-02-05 14:59:27 +0545

    url hide

--- config/database.py
+++ config/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os 
+from dotenv import load_dotenv
 
-DATABASE_URL = "mysql+pymysql://root:Betternow10%23@localhost:3306/reportdb"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(url=DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
