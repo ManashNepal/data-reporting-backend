a05d372133c766188c0e761eb1cb325d0f6bd03a
ManashNepal <manash.nepal111@gmail.com>
2026-02-10 12:11:30 +0545

    remove testing files

--- app/schemas/query.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, Field
-from typing import Dict, Any, Optional
-
-class QueryRequest(BaseModel):
-    query : str = None
-    params : Dict[str, Any] = Field(default_factory=dict) 
-    connection_id : str
-    
--- server.py
+++ server.py
@@ -6,7 +6,6 @@ from app.models.report_columns import ReportColumn
 
 from routes.report_route import router as report_router
 from routes.auth_route import router as auth_router
-from routes.query_route import router as query_router
 from routes.connection_route import router as connection_router
 from routes.query_executor import router as query_executor_router
 from routes.report_column_queries import router as report_column_queries_router
@@ -23,7 +22,6 @@ def health():
 
 app.include_router(report_router)
 app.include_router(auth_router)
-app.include_router(query_router)
 app.include_router(connection_router)
 app.include_router(query_executor_router)
 app.include_router(report_column_queries_router)
